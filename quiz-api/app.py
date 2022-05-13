9bda06eaa73667c0c709dd03612891c00d2bd32c
UHDP <56586866+UHDP@users.noreply.github.com>
2022-05-13 14:34:29 +0200

    login endpoint

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from jwt_utils import build_token
 
 app = Flask(__name__)
 
@@ -10,6 +11,24 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # username = "admin"
+    password = "Vive l'ESIEE !"
+
+    payload = request.get_json()
+
+    if("password" not in payload.keys()):
+        return {"Error": 400, "message": "must indicate a password"}, 400
+
+    if(password != payload["password"]):
+        return {"Error": 401, "message": "Wrong password"}, 401
+
+    token = build_token()
+
+    return {"access_token": token}
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
